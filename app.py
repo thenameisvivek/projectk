9d751862d2dcb8532edcd9986c66f36f5d36def3
thenameisvivek <34vivek36sindham@gmail.com>
2025-04-10 18:26:26 +0530

    updtaed app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ def lambda_handler(event, context):
     client = boto3.client('ec2')
     response = client.run_instances(
        ImageId= 'ami-0614680123427b75e',
-       KeyName= 'mykeymumbai'
+       KeyName= 'aws'
        MaxCount=1,
        mincount=1
        )
