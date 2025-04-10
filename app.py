4c63293bf8b16a717bcdf38ea83b7041788d36aa
thenameisvivek <159502546+thenameisvivek@users.noreply.github.com>
2025-04-10 18:05:19 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+    client = boto3.client('ec2')
+    response = client.run_instances(
+       ImageId= 'ami-0614680123427b75e',
+       KeyName= 'mykeymumbai'
+       MaxCount=1,
+       mincount=1
+       )
+
